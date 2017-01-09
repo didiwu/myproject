8849a9bf871678bddda4a4993ecd72f64a41f6e9
root <root@ip-172-31-7-25.ap-southeast-2.compute.internal>
2017-01-09 17:41:18 -0500

    Updated index page

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
-<html>
-    <head>
-        <title>Hello World!</title>
-    </head>
-    <body>
-        <?php echo '<h1>Hello World from gethostname()</h1>'; ?>
-    </body>
-</html>
+<?php 
+
+echo "Hello from ".gethostname() 
+
+?>
